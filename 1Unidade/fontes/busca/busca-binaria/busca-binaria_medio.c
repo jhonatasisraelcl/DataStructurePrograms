85a4169b0d372c124b296bacde84c5df945264bc
Jhonatas <costajhonatas9@gmail.com>
2019-04-22 02:09:05 -0300

    Update busca-binaria_medio.c

--- 1Unidade/fontes/busca/busca-binaria/busca-binaria_medio.c
+++ 1Unidade/fontes/busca/busca-binaria/busca-binaria_medio.c
@@ -6,45 +6,35 @@
 #include <time.h>
 #include <unistd.h>
 
-
-
 int *newv (int n);
 double tvtosec(struct timeval t);
- int search(int *v, int s, int e, int k);
+int search(int *v, int s, int e, int k);
 
-int main(void) 
- {
-
+int main(void){
   srand(time(NULL)); 
   struct timeval a;
   struct timeval b;
   double tempo;
   int n,i;
 
-
   for(n = 10000; n <= 100000; n+= 1000){
       tempo = 0;
-
       for(i = 0; i < 20000; i++){
-
           
         int *v = newv(n);
         int procurar = rand() % n;
         gettimeofday(&b, NULL);
-        int achou = search(v, 0, n-1, procurar); /* caso médio será procurado um número aleatório dentro do vetor */
+        int achou = search(v, 0, n-1, procurar);
         gettimeofday(&a, NULL);
         tempo  += tvtosec(a) - tvtosec(b);
         free(v);
-      }
-      
+      }  
     fprintf(stderr, "%d %.15lf\n", n, tempo/20000 );
     printf("%d %.15lf\n", n, tempo/20000);
   }
-
 }
 
  int search(int *v, int s, int e, int k){
-
   int m = (s+e)/2;
   if(s <= e){
     if(v[m] == k){
@@ -57,23 +47,14 @@ int main(void)
   }else{
     return -1;
   }
-
  }
 
-
  int *newv (int n){
-
   int *v =  (int*)malloc(n*sizeof(int));
-  for(int i = 0; i < n; i++)
-  { v[i] = i+1;}
-
-
+  for(int i = 0; i < n; i++){ v[i] = i+1;}
   return v;
 }
 
-
 double tvtosec(struct timeval t){
   return (double) t.tv_sec + t.tv_usec/(double)1e6;
-
 }
-

4f2e5d35398939852c6653518298dddb542e89a2
Jhonatas <costajhonatas9@gmail.com>
2019-04-22 02:20:31 -0300

    Update busca-linear_melhor.c

--- 1Unidade/fontes/busca/busca-linear/busca-linear_melhor.c
+++ 1Unidade/fontes/busca/busca-linear/busca-linear_melhor.c
@@ -1,3 +1,6 @@
+//TITULO DO PROJETO:busca-linear_melhor.c
+//VERSÃO ou DATA:1.0 27/02/2019
+//AUTORES:LAURENTINO, J.I.C.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,50 +9,35 @@
 #include <time.h>
 #include <unistd.h>
 
-
-
 int *newv (int n);
 double tvtosec(struct timeval t);
 int search(int *v, int n, int x);
 
-int main(void) 
- {
-
+int main(void) {
   srand(time(NULL)); 
   struct timeval a;
   struct timeval b;
   double tempo;
   int n,i;
-
-
   for(n = 10000; n <= 100000; n+= 1000){
       tempo = 0;
-
       for(i = 0; i < 30000; i++){
-
-   
         int *v = newv(n);
         gettimeofday(&b, NULL);
-        int achou = search(v, n-1, 10); /* no melhor caso o elemento procurado será semrpe o 10, que estará na primeira posição */
+        int achou = search(v, n-1, 10); 
         gettimeofday(&a, NULL);
         tempo  += tvtosec(a) - tvtosec(b);
         free(v);
-      }
-      
+      }  
     fprintf(stderr, "%d %.15lf\n", n, tempo/50000 );
     printf("%d %.15lf\n", n, tempo/50000);
   }
-
   return 0;
-
  }
 
-
  int search(int *v, int n, int x){
-
   int i;
-  for (i = 0; i < n; i++)
-  {
+  for (i = 0; i < n; i++){
     if(v[i] == x){
       return i;
     }
@@ -57,22 +45,13 @@ int main(void)
   return -1;
  }
 
-
  int *newv (int n){
-
   int *v =  (int*)malloc(n*sizeof(int));
-  for(int i = 0; i < n; i++)
-  { v[i] = i+1;}
-
+  for(int i = 0; i < n; i++){ v[i] = i+1;}
   v[0] = 10;
-
-
   return v;
 }
 
-
 double tvtosec(struct timeval t){
-  return (double) t.tv_sec + t.tv_usec/(double)1e6;
-
+  return (double) t.tv_sec + t.tv_usec/(double);
 }
-

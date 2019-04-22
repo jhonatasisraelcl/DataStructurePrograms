2143dce8de71f70db68c7d283fe144e524185a07
Jhonatas <costajhonatas9@gmail.com>
2019-04-22 02:14:05 -0300

    Update busca-binaria_melhor.c

--- 1Unidade/fontes/busca/busca-binaria/busca-binaria_melhor.c
+++ 1Unidade/fontes/busca/busca-binaria/busca-binaria_melhor.c
@@ -1,3 +1,6 @@
+//TITULO DO PROJETO:busca-binaria_melhor.c
+//VERSÃO ou DATA:1.0 26/02/2019
+//AUTORES:LAURENTINO, J.I.C.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,42 +9,30 @@
 #include <time.h>
 #include <unistd.h>
 
-
-
 int *newv (int n);
 double tvtosec(struct timeval t);
- int search(int *v, int s, int e, int k);
+int search(int *v, int s, int e, int k);
 
-int main(void) 
- {
-
+int main(void){
   srand(time(NULL)); 
   struct timeval a;
   struct timeval b;
   double tempo;
   int n,i, achou;
-
-
   for(n = 10000; n <= 100000; n+= 1000){
       tempo = 0;
-
       for(i = 0; i < 10000; i++){
-
-   
         int *v = newv(n);
         gettimeofday(&b, NULL);
-        int achou = search(v, 0, n-1, 10); /* no melhor caso o está na posição do meio */
+        int achou = search(v, 0, n-1, 10);
         gettimeofday(&a, NULL);
         tempo  += tvtosec(a) - tvtosec(b);
         free(v);
-      }
-      
+      } 
     fprintf(stderr, "%d %.15lf\n", n, tempo/10000 );
     printf("%d %.15lf\n", n, tempo/10000);
   }
-
 }
-
  int search(int *v, int s, int e, int k){
   int m = (s+e)/2;
   if(s <= e){
@@ -57,24 +48,14 @@ int main(void)
   }
  }
 
-
  int *newv (int n){
-
   int *v =  (int*)malloc(n*sizeof(int));
-  for(int i = 0; i < n; i++)
-  { v[i] = i+1;}
-
+  for(int i = 0; i < n; i++){ v[i] = i+1;}
    n--;
    int meio = n/2;
    v[meio] = 10;
-
-
   return v;
 }
-
-
 double tvtosec(struct timeval t){
-  return (double) t.tv_sec + t.tv_usec/(double)1e6;
-
+  return (double) t.tv_sec + t.tv_usec/(double);
 }
-

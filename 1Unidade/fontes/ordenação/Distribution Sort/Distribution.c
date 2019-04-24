d40932591730d301f094b950b2e9cd1fd343c2cc
Jhonatas <costajhonatas9@gmail.com>
2019-04-24 14:45:07 -0300

    Update Distribution.c

--- 1Unidade/fontes/ordenação/Distribution Sort/Distribution.c
+++ 1Unidade/fontes/ordenação/Distribution Sort/Distribution.c
@@ -1,3 +1,6 @@
+//TITULO DO PROJETO:Distribution.c
+//VERSÃO ou DATA:1.3 08/03/2019
+//AUTORES:LAURENTINO, J.I.C.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,10 +16,8 @@ int *new_0 (unsigned int n);
 int min(int *v, int n);
 int max(int *v, int n);
 
-
 int main(void) 
  {
-
 	srand(time(NULL)); 
 	struct timeval a;
 	struct timeval b;
@@ -24,28 +25,20 @@ int main(void)
 	int *ordenado;
 
  	for(int n = 1000; n <= 10000; n += 100){
-
  		tempo = 0;
-
  		for(int i = 0; i < 1000 ; i++){
- 
 		 	int *v = newv(n);
-
 		 	gettimeofday(&b, NULL);
 		 	ordenado = distribution(v, n);
 		 	gettimeofday(&a, NULL);
-
 		 	tempo  += tvtosec(a) - tvtosec(b);
 		 	free(v);
 	 	}
-
 	 	printf("%d %10lf\n", n, tempo/1000);
 	 	fprintf(stderr, "%d %10lf\n", n, tempo/1000 );
-
 	 }
 
  	return 0;
-
  }
 
  int *distribution (int *v, int n){
@@ -55,78 +48,49 @@ int main(void)
  	int i, j;
  	int *w = new_0(k);
  	int *y = new_0(n);
-
-     for (i = 0;i < n ;i++){
-          w[v[i]-l]++;
-     }
-
-     for (j = 1; j <= b-l;j++){
-         w[j] = w[j] + w[j-1];
-     }
-
+     for (i = 0;i < n ;i++){ w[v[i]-l]++;}
+     for (j = 1; j <= b-l;j++){w[j] = w[j] + w[j-1];}
      for (i = 0;i <= n-1; i++){
          j = w[v[i]- l];
          y[j-1] = v[i];
          w[v[i]-l]--;
      }
-
  	return y;
  	free(y);
  	free(w);
  }
 
  int *newv (unsigned int n){
-
   int *v =  (int*)malloc(n*sizeof(int));
   for(int i = 0; i < n; i++){ v[i] = rand() % n ;}
-
-
   return v;
-
 }
 
  int *new_0 (unsigned int n){
-
   int *v =  (int*)malloc(n*sizeof(int));
   for(int i = 0; i < n; i++) v[i] = 0;
-
-
   return v;
-
 }
 
 double tvtosec(struct timeval t){
   return (double) t.tv_sec + t.tv_usec/(double)1e6;
-
 }
 
 int min(int *v, int n){
 	int menor;
 	for(int i = 0; i < n; i++){
-		if(i == 0){
-			menor = v[i];
-		}else{
-			if(v[i] < menor){
-				menor = v[i];
-			}
-		}
+		if(i == 0){menor = v[i];}
+		else{if(v[i] < menor){menor = v[i];}}
 	}
-
 	return menor;
 }
 
 int max(int *v, int n){
 	int maior;
 	for(int i = 0; i < n; i++){
-		if(i == 0){
-			maior = v[i];
-		}else{
-			if(v[i] > maior){
-				maior = v[i];
-			}
+		if(i == 0){maior = v[i];}
+		else{if(v[i] > maior){maior = v[i];}
 		}
 	}
-
 	return maior;
 }
-

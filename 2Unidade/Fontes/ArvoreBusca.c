eb55c6212e10e2e80ca583425db4cf5b04588af5
Jhonatas <costajhonatas9@gmail.com>
2019-05-14 15:34:44 -0300

    Update ArvoreBusca.c

--- 2Unidade/Fontes/ArvoreBusca.c
+++ 2Unidade/Fontes/ArvoreBusca.c
@@ -10,7 +10,6 @@ typedef struct node {
     int v;
     struct node *l;
     struct node *r;
-
 }Node;
 
 void adicionar(Node **node, int v);
@@ -20,7 +19,6 @@ int *newv (unsigned int n);
 double tvtosec(struct timeval t);
 void tprint(Node* x);
 void tremove(Node* x);
-
  
 int main (void){
     Node *node = NULL;
@@ -34,25 +32,21 @@ int main (void){
  	for(n = 1000; n <= 10000; n += 500){
  		tempo = 0;        
  		for(i = 0; i < 10000 ; i++){
-            for(k = 0; k < n; k++){
-                aleatorio = rand() % (n+1);
-                adicionar(&node, aleatorio);
-                if(k == 0){
-                    primeiro = aleatorio;
-                }
-            }
-
+            		for(k = 0; k < n; k++){
+                		aleatorio = rand() % (n+1);
+                		adicionar(&node, aleatorio);
+                		if(k == 0){
+                    			primeiro = aleatorio;
+                		}
+            		}
 		 	gettimeofday(&b, NULL);
-
-            achou = tsearch(node, primeiro); 
-            achou = tsearch(node, (n*2)); 
-            achou = tsearch(node, (rand() % (n+1))); 
+		    	achou = tsearch(node, primeiro); 
+		    	achou = tsearch(node, (n*2)); 
+		    	achou = tsearch(node, (rand() % (n+1))); 
 		 	gettimeofday(&a, NULL);
-
-		 	tempo  += tvtosec(a) - tvtosec(b);
-		 	            
-            tremove(node);
-            node = NULL;
+		 	tempo  += tvtosec(a) - tvtosec(b);	            
+            		tremove(node);
+            		node = NULL;
 	 	}
 	 	fprintf(stderr, "%d %.20lf\n", n, tempo/10000 );
 	 	printf("%d %.20lf\n", n, tempo/10000 );
@@ -62,73 +56,49 @@ int main (void){
 
 void adicionar(Node **n, int v) {
     Node *aux, *f;
-    if(*n == NULL){
-        (*n) = newnode(v);
-    }else{
+    if(*n == NULL){(*n) = newnode(v);}
+	else{
         aux = *n;
         while(aux != NULL){
             f = aux;
-            if(aux->v < v){
-                aux = aux->r;
-            }else{
-                aux = aux->l;
-            }
+            if(aux->v < v){aux = aux->r;}
+		else{aux = aux->l;}
         }
-
       (f->v < v) ? (f->r = newnode(v)) : (f->l = newnode(v));
     }
 }
 
-
 int tsearch(Node* x, int v){
     if(x != NULL){
-        if(x->v == v){
-            return 1;
-        }else if(x->v < v){
-            return tsearch(x->r, v);
-        }else{ 
-            return tsearch(x->l,v);
-        }
-    }else{
-        return 0;
-    }
-
+        if(x->v == v){return 1;}
+        else if(x->v < v){return tsearch(x->r, v);}
+        else{return tsearch(x->l,v);}
+    }else{return 0;}
 }
 
 Node* newnode(int num){
-
     Node* a = (Node*)malloc(sizeof(Node));
     a->v = num;
     a->l = NULL;
     a->r = NULL;
     return a;
-
 }
 
+double tvtosec(struct timeval t){return (double) t.tv_sec + t.tv_usec/(double)1e6;}
 
-double tvtosec(struct timeval t){
-  return (double) t.tv_sec + t.tv_usec/(double)1e6;
-
-}
-
-
-void tprint(Node* x){
-    
+void tprint(Node* x){ 
     if(x != NULL){
         tprint(x->l);
         printf("%p %d %p %p \n", x , x->v, x->l, x->r);
         tprint(x->r);
     }
-
 }
 
 void tremove(Node* x){
-
     if(x != NULL){
         tremove(x->l);
         tremove(x->r);
         free(x);
     }
     return;
-
 }
